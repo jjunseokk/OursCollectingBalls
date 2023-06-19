e17ad939e886e4524da52af97908e5ed49643fbc
Ubuntu <ubuntu@ip-172-31-1-170.ap-northeast-2.compute.internal>
2023-06-19 00:16:38 +0000

    server port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const http = require("http");
-const port = 5001;
+const port = 5000;
 
 require('dotenv').config();
 
