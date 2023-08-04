e4ae7b093d84ecfb629a784e82d952ae532cf6d5
jjunseokk <p.junseok53@gmail.com>
2023-08-04 22:07:05 +0900

    server port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const http = require("http");
-const port = 80;
+const port = 5000;
 
 require('dotenv').config();
 
